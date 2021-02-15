489b2ee6854b25c48cf7b3140ef1a449ff535b5c
Raphael Cerqueira <rcerqueira@vr.com.br>
2021-02-15 13:09:03 -0300

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Raphael Cerqueira on 15/02/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
